b3e31a9fdb11fa5e673f5c60b76bc00fa7afc598
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-01-13 23:55:17 -0500

    add average assistance time, average cohort assistance,cohort longest assistance, average request assistance, total cohort assistance

---
+++ 4_queries/4_average_cohort_assistance_time.sql
@@ -0,0 +1,33 @@
+-- SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+-- FROM assistance_requests
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- ORDER BY average_assistance_time;
+
+
+SELECT cohorts.name,
+AVG(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_time 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+JOIN assistance_requests ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time
+
+
+-- SELECT cohorts.name, 
+--        AVG(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_time
+-- FROM cohorts
+-- JOIN students ON cohorts.id = students.cohort_id
+-- JOIN assistance_requests ON students.id = assistance_requests.student_id
+-- GROUP BY cohorts.name
+-- ORDER BY average_assistance_time;
+
+
+
+-- SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+-- FROM assistance_requests 
+-- JOIN students ON students.id = assistance_requests.student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- ORDER BY average_assistance_time;

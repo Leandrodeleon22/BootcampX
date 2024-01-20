4be09022fd9519caa5d861d742f225f7e62eec3e
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-01-20 10:21:39 -0500

    sql injections

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -8,16 +8,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const queryString = `
+
 SELECT students.id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${data[0]}%'
-LIMIT ${data[1]};
-`
-  )
+WHERE cohorts.name LIKE '$1'
+LIMIT $2
+
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
   .then((res) => {
     // console.log(res.rows);
     res.rows.forEach((user) => {
